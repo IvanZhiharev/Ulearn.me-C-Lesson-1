ff992c6410347eaf59a2643069d202fac29738a5
Ivan Zhikharev <pssr86@inbox.ru>
2019-07-26 08:55:10 +0500

    first save

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulearn___1_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Решения заданий для семминара после первой лекции курса по программированию на сайте Ulearn.me");
+            Console.WriteLine("Автор - Жихарев И.Д.");
+            Console.WriteLine("Всего 8 заданий. Чтобы посмотреть решение конкретного задания - нажмите на клавиатуре соотвествующую цифру (1-8)");
+
+        }
+        public static void NomerZad()
+        {
+
+        }
+    }
+}
